13d3257114511ebcd508c9fb64c7c39aad58eb92
Pavan <pavan.kumar@dakoo.app>
2023-11-17 11:45:21 +0530

    new

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import userSlice from "./features/userSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: userSlice,
+  },
+});
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const primary = "#5a5bb2";
+
+export const secondary = "#91a1b7";
+
+// export const textColor =
+
+// #fcfcfc
+
+// #6d6942
+// #5653b5
