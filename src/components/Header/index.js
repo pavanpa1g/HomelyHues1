71ae6bafeeee6afe66fdfdecfdf80fd7d12818f0
Pavan Kumar <64405345+pavanpa1g@users.noreply.github.com>
2024-03-02 14:12:57 +0530

    fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -24,7 +24,7 @@ const Header = () => {
   const onClickLogout = () => {
     localStorage.removeItem("userData");
     Cookies.remove("jwt_token");
-    router.replace("/");
+    router.replace("/login");
   };
 
   const pathName = usePathname();
